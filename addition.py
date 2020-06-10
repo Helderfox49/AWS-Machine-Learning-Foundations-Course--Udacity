367924bf2800a1d5a3152d49059b36a0b425511d
Helderfox49 <Helderfox49@users.noreply.github.com>
2020-06-10 10:21:02 +0100

    Add files via upload
    I write a basic function to implement unit test.

---
+++ addition.py
@@ -0,0 +1,8 @@
+def addition(x,y):
+    """This function returns the sum of the numbers
+
+    x (int) - first number in integer
+    y (int) - second number in integer
+    """
+
+    return x + y
---
+++ test_addition.py
@@ -0,0 +1,10 @@
+from addition import addition
+
+def test_addition_2():
+    assert(addition(1,1) == 2)
+
+def test_addition_6():
+    assert(addition(2,4) == 6)
+
+def test_addition_10():
+    assert(addition(6,4) == 10)
